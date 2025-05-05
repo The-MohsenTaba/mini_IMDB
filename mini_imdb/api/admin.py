44e6f65c64acdcb8bddd45454c59b11dc58780d4
mohsentaba <s.hosseintaba98@gmail.com>
2025-05-05 11:45:06 +0330

    updated models with more detailed data

--- mini_imdb/api/admin.py
+++ mini_imdb/api/admin.py
@@ -3,7 +3,8 @@ from api.models import*
 from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 class MovieAdmin(admin.ModelAdmin):
-    list_display=['title','rating_count','average_rating']
+    list_display=['title','rating_count','average_rating','genere']
 admin.site.register(User,UserAdmin)
 admin.site.register(Movie,MovieAdmin)
 admin.site.register(Vote)
+admin.site.register(Person)
--- mini_imdb/api/models.py
+++ mini_imdb/api/models.py
@@ -1,11 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class Person(models.Model):
+    first_name = models.CharField(null=False,max_length=50)
+    last_name = models.CharField(null=False,max_length=50)
+
+    def __str__(self):
+        return self.first_name + " " + self. last_name
+    
+
 class Movie (models.Model):
+    class GenereChoices(models.TextChoices):
+        ACTION="Action",
+        COMEDY="Comedy"
+        DRAMA="Drama"
+        HORROR="Horror"
+        ROMANCE="Romance"
+        BIOGRAPHY="Biography"
+        SCIFI="Sci-fi"
+        THRILLER="Thriller"
     title= models.CharField(null=False,max_length=50)
     year = models.IntegerField(null=True)
+    genere= models.CharField(null=True,choices=GenereChoices,max_length=50)
     average_rating=models.FloatField(default=0)
     rating_count=models.PositiveIntegerField(default=0)
+    directors = models.ManyToManyField(Person,related_name="directors")
+    actors = models.ManyToManyField(Person,related_name="actors")
     def update_rating_stats(self):
         from django.db.models import Avg,Count
         stats = self.vote_set.aggregate(
@@ -20,6 +42,8 @@ class User(AbstractUser):
     pass
     def __str__(self):
         return self.username
+    
+
 
 class Vote(models.Model):
     rating = models.DecimalField(max_digits=2,decimal_places=1)
